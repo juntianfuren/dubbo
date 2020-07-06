ddf43428446a058c1111f45f168a356cc83ea3c6
juntianfuren <1123181523@qq.com>
2020-07-06 23:37:42 +0800

    sentinel项目建立

---
+++ dubbo-provider-sentinel-v1/src/main/java/com/jtfr/service/HelloServiceImpl.java
@@ -0,0 +1,14 @@
+package com.jtfr.service;
+
+import com.jtfr.HelloService;
+import org.apache.dubbo.config.annotation.Service;
+
+@Service(version = "${dubbo.version}")
+public class HelloServiceImpl implements HelloService {
+
+	@Override
+	public String sayHello(String name) {
+		return "Hello " + name +" sentinel v1.0";
+	}
+
+}
